dfbd747f28a0ee0e44f4f96b71024e88ee19cdcf
Aliaksei <gasumta@gmail.com>
2025-10-06 14:40:20 +0200

    Add tests

---
+++ Patient.Domain.Tests/Implementation/PatientBossServices/WhenRemovePatient.cs
@@ -0,0 +1,22 @@
+﻿using DbDataAccess.Abstractions;
+
+namespace Patient.Domain.Tests.Implementation.PatientBossServices
+{
+    public class WhenRemovePatient : BasePatientBossServiceFixture
+    {
+        private DomainModels.Patient model;
+
+        public WhenRemovePatient() 
+        {
+            model = new DomainModels.Patient();
+            Subject.Delete(model.Id, CancellationToken.None);
+        }
+
+        [Fact]
+        public void ShouldCallRepo() 
+        {
+            mocker.GetMock<IPatientRepository>()
+                .Verify(x => x.Delete(model.Id, CancellationToken.None));
+        }
+    }
+}
