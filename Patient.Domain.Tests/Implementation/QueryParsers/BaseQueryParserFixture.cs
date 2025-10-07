f1684508998d27d1174643e9b6af8092bbeb4889
Aliaksei <gasumta@gmail.com>
2025-10-07 14:35:07 +0200

    add tests for query parsing

---
+++ Patient.Domain.Tests/Implementation/QueryParsers/BaseQueryParserFixture.cs
@@ -0,0 +1,17 @@
+﻿using Moq.AutoMock;
+using Patient.Domain.Implementation;
+
+namespace Patient.Domain.Tests.Implementation.QueryParsers
+{
+    public class BaseQueryParserFixture
+    {
+        protected AutoMocker autoMocker = new AutoMocker();
+
+        protected QueryParser Subject;
+
+        protected BaseQueryParserFixture() 
+        {
+            Subject = autoMocker.CreateInstance<QueryParser>();
+        }
+    }
+}
---
+++ Patient.Domain.Tests/Implementation/QueryParsers/WhenParseQuery.cs
@@ -0,0 +1,29 @@
+﻿using Patient.DomainModels.QueryParse;
+using Shouldly;
+
+namespace Patient.Domain.Tests.Implementation.QueryParsers
+{
+    public class WhenParseQuery : BaseQueryParserFixture
+    {
+        ParseResult parseResult;
+
+        public WhenParseQuery() 
+        {
+            parseResult = Subject.Parse("eq1999-12-12T12:12:12");
+        }
+
+        [Fact]
+        public void ShouldReturnData() 
+        {
+            parseResult.Time.ShouldBe(new TimeOnly(12, 12, 12));
+            parseResult.Date.ShouldBe(new DateOnly(1999, 12, 12));
+            parseResult.Prefix.ShouldBe(Prefix.Equal);
+        }
+
+        [Fact]
+        public void ShouldBeSuccesfull() 
+        {
+            parseResult.Status.ShouldBe(Status.Success);
+        }
+    }
+}
