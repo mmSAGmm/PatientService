afbb93c5fca94a0faf236e76c78ee3d6ae089fc8
Aliaksei <gasumta@gmail.com>
2025-10-08 07:36:10 +0200

    add failed tests

---
+++ Patient.Domain.Tests/Implementation/QueryParsers/WhenParseFailedQuery.cs
@@ -0,0 +1,21 @@
+﻿using Patient.DomainModels.QueryParse;
+using Shouldly;
+
+namespace Patient.Domain.Tests.Implementation.QueryParsers
+{
+    public class WhenParseFailedQuery : BaseQueryParserFixture
+    {
+        ParseResult parseResult;
+
+        public WhenParseFailedQuery() 
+        {
+            parseResult = Subject.Parse("asaas");
+        }
+
+        [Fact]
+        public void ShouldBeFailed() 
+        {
+            parseResult.Status.ShouldBe(Status.Failed);
+        }
+    }
+}
