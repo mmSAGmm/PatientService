5a94b03abe5e8a21b68eff69d47d6f5ea75f1d40
Aliaksei <gasumta@gmail.com>
2025-10-10 15:33:06 +0200

    add metrics and traces

--- Patient.Domain.Tests/Implementation/PatientBossServices/WhenAddPatient.cs
+++ Patient.Domain.Tests/Implementation/PatientBossServices/WhenAddPatient.cs
@@ -13,7 +13,7 @@ namespace Patient.Domain.Tests.Implementation.PatientBossServices
         }
 
         [Fact]
-        public void ShouldCallRepo() 
+        public void ShouldCallRepo()
         {
             mocker.GetMock<IPatientRepository>()
                 .Verify(x => x.Add(model, CancellationToken.None));
--- src/PatientService/Program.cs
+++ src/PatientService/Program.cs
@@ -1,4 +1,6 @@
 using DbDataAccess;
+using OpenTelemetry.Metrics;
+using OpenTelemetry.Trace;
 using Patient.Domain;
 using PatientService.HostedServices;
 using PatientService.Middleware;
@@ -11,8 +13,30 @@ builder.Services.AddSwaggerGen();
 builder.Services.AddOptions();
 builder.Services.AddHostedService<CrawlerService>();
 
+builder.Services.AddOpenTelemetry()
+       .WithTracing(builder => builder
+           .AddAspNetCoreInstrumentation()
+           .AddHttpClientInstrumentation()
+           //.AddConsoleExporter()
+           
+           )
+        .WithMetrics(builder => builder
+            .AddAspNetCoreInstrumentation()
+            .AddMeter("Microsoft.AspNetCore.Hosting")
+            .AddMeter("Microsoft.AspNetCore.Server.Kestrel")
+            // Metrics provided by System.Net libraries
+            .AddMeter("System.Net.Http")
+            .AddMeter("System.Net.NameResolution")
+
+            .AddHttpClientInstrumentation()
+          //  .AddConsoleExporter()
+            .AddPrometheusExporter())
+        .WithLogging(builder => { });
+
+
+
 builder.Services
-    .AddControllers(c=> c.Filters.Add(new ValidationFilterAttribute()));
+    .AddControllers(c => c.Filters.Add(new ValidationFilterAttribute()));
 
 builder.Services
     .AddDomain()
@@ -26,5 +50,6 @@ app.UseSwagger();
 app.UseSwaggerUI();
 
 app.MapControllers();
+app.MapPrometheusScrapingEndpoint();
 
 app.Run();
