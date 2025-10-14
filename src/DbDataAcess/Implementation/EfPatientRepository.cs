78ba02fb620cacfa45ea91970ca791e798afc909
Aliaksei <gasumta@gmail.com>
2025-10-14 16:52:03 +0200

    add  search implementation

--- src/DbDataAcess/Implementation/EfPatientRepository.cs
+++ src/DbDataAcess/Implementation/EfPatientRepository.cs
@@ -1,5 +1,7 @@
 ﻿using Db.DataAccess.EF;
 using DbDataAccess.Abstractions;
+using Microsoft.EntityFrameworkCore;
+using MySql.Data.MySqlClient;
 using Patient.DomainModels.QueryParse;
 using System.Text.Json;
 
@@ -7,6 +9,9 @@ namespace Db.DataAccess.Implementation
 {
     public class EfPatientRepository(DbContex dbContext) : IPatientRepository
     {
+        private const string dbFormat = "yyyy-MM-dd HH:mm:ss";
+
+        private const string dateFormat = "yyyy-MM-dd";
 
         public async Task Add(Patient.DomainModels.Patient model, CancellationToken token)
         {
@@ -42,9 +47,85 @@ namespace Db.DataAccess.Implementation
             return null;
         }
 
-        public Task<IEnumerable<Patient.DomainModels.Patient>> Search(IEnumerable<ParseResult> parseResults, CancellationToken token)
+        public async Task<IEnumerable<Patient.DomainModels.Patient>> Search(IEnumerable<ParseResult> parseResults, CancellationToken token)
         {
-            throw new NotImplementedException();
+            List<string> queries = new List<string>();
+            List<MySqlParameter> parameters = new List<MySqlParameter>();
+
+            for (var i = 0; i < parseResults.Count(); i++)
+            {
+                var pR = parseResults.ElementAt(i);
+                var op = "";
+                op = pR.Prefix switch
+                {
+                    Prefix.Equal => "=",
+                    Prefix.LessThan => "<",
+                    Prefix.GraterThan => ">",
+                    Prefix.GreaterOrEqual => ">=",
+                    Prefix.LessOrEqual => "<=",
+                    Prefix.StartsAfter => ">",
+                    Prefix.EndBefore => "<",
+                    Prefix.Approximately => "ap",
+                    _ => throw new InvalidOperationException($"{pR.Prefix} is unknown")
+                };
+
+
+                if (pR.Prefix == Prefix.Approximately)
+                {
+                    if (pR.Date.HasValue)
+                    {
+                        queries.Add($"birthDate > @pg{i} AND birthDate < @pl{i}");
+
+                        var dtUnix = new DateTimeOffset(new DateTime(
+                                pR.Date.Value.Year,
+                                pR.Date.Value.Month,
+                                pR.Date.Value.Day,
+                                pR.Time.HasValue ? pR.Time.Value.Hour : 0,
+                                pR.Time.HasValue ? pR.Time.Value.Minute : 0,
+                                pR.Time.HasValue ? pR.Time.Value.Second : 0)).ToUnixTimeSeconds();
+
+                        AddParameter(parameters, $"pg{i}", DateTimeOffset.FromUnixTimeSeconds((long)(dtUnix * 0.9))
+                            .ToString(dbFormat));
+                        AddParameter(parameters, $"pl{i}", DateTimeOffset.FromUnixTimeSeconds((long)(dtUnix * 1.1))
+                            .ToString(dbFormat));
+                    }
+
+                    continue;
+                }
+
+                if (pR.Date.HasValue && pR.Time.HasValue)
+                {
+                    queries.Add($"birthDate {op} @p{i}");
+                    AddParameter(parameters, $"p{i}",
+                        new DateTime(
+                            pR.Date.Value.Year,
+                            pR.Date.Value.Month,
+                            pR.Date.Value.Day,
+                            pR.Time.Value.Hour,
+                            pR.Time.Value.Minute,
+                            pR.Time.Value.Second).ToString(dbFormat));
+                }
+                else if (pR.Date.HasValue && !pR.Time.HasValue)
+                {
+                    queries.Add($"DATE(birthDate) {op} @p{i}");
+                    AddParameter(parameters, $"p{i}",
+                      new DateTime(
+                          pR.Date.Value.Year,
+                          pR.Date.Value.Month,
+                          pR.Date.Value.Day).ToString(dateFormat));
+                }
+            }
+
+            if (!queries.Any())
+            {
+                throw new InvalidOperationException("No conditions for search");
+            }
+
+            var users = await dbContext.Patients
+                        .FromSqlRaw($"SELECT id, json, birthDate FROM tbPatients WHERE {string.Join(" AND ", queries)}", parameters.ToArray())
+                        .ToListAsync();
+
+            return users;
         }
 
         public async Task Update(Patient.DomainModels.Patient model, CancellationToken token)
@@ -60,6 +141,14 @@ namespace Db.DataAccess.Implementation
                 entity.Given = model.Given;
                 await dbContext.SaveChangesAsync();
             }
+        }
+
+        private void AddParameter(List<MySqlParameter> parameters, string name, object value)
+        {
+            var idParameter = new MySqlParameter();
+            idParameter.ParameterName = name;
+            idParameter.Value = value.ToString();
+            parameters.Add(idParameter);
         }
     }
 }
